f3b95e324d60db201a291ec942a3c85d4fd465b5
Michael Krut <krutmj97@gmail.com>
2022-11-29 17:13:19 -0500

    started main method CLI

---
+++ src/main/java/com/techelevator/MainCLI.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import java.io.FileNotFoundException;
+
+public class MainCLI {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArtistDirectory buildDirectory = new ArtistDirectory("local_entertainment.txt");
+
+
+
+
+    }
+}
